13d6686e8261c441fafcd03763f1f42f46440b5e
Luke <luke.e.philips@gmail.com>
2016-12-20 18:53:34 -0800

    fix suffix-strip method

--- lib/sentence.rb
+++ lib/sentence.rb
@@ -7,45 +7,24 @@ class String
     sentence_array.each do |word|
       if !remove_list.include?(word)
         word = word.gsub(/[^0-9A-Za-z]/, '')
-        keyword_hash = Keyword.where("keyword LIKE LOWER(?)", "#{word}").first
+        keyword_hash = Keyword.where("LOWER(keyword) LIKE LOWER(?)", "#{word}").first
+
         test_word = word
         test_hash = keyword_hash
-        if test_hash == nil
-          # if test_word.end_with?("ing") or test_word.end_with?("ed") or test_word.end_with?("s") or test_word.end_with?("er")
-            while test_word.end_with?("ing") or test_word.end_with?("ed") or test_word.end_with?("s") or test_word.end_with?("er")
-              if test_word.end_with?('ing')
-                test_word = word.chomp('ing')
-                test_hash = Keyword.where("keyword LIKE LOWER(?)", "#{test_word}").first
+
+        if keyword_hash == nil # if nothing is returned when testing the whole word
+          suffix = ['ing','ed','s','er']
+          suffix.each do |suffix|
+            if test_word.end_with?(suffix)
+                test_hash = Keyword.where("LOWER(keyword) LIKE LOWER(?)", "#{test_word.chomp(suffix)}").first
                 if test_hash
-                  @return.push(Emoji.find(test_hash[:emoji_id]).image)
-                end
-              elsif test_word.end_with?('ed')
-                test_word= word.chomp('ed')
-                test_hash = Keyword.where("keyword LIKE LOWER(?)", "#{test_word}").first
-                if test_hash
-                  @return.push(Emoji.find(test_hash[:emoji_id]).image)
-                end
-              elsif test_word.end_with?('s')
-                test_word = word.chomp('s')
-                test_hash = Keyword.where("keyword LIKE LOWER(?)", "#{test_word}").first
-                if test_hash
-                  @return.push(Emoji.find(test_hash[:emoji_id]).image)
-                end
-              elsif test_word.end_with?('er')
-                test_word = word.chomp('er')
-                test_hash = Keyword.where("keyword LIKE LOWER(?)", "#{test_word}").first
-                if test_hash
-                  @return.push(Emoji.find(test_hash[:emoji_id]).image)
+                  word = (Emoji.find(test_hash[:emoji_id]).image)
                 end
               end
-            end
-          # else
+          end
             @return.push(word)
-          # end
         else
-          emoji_id = keyword_hash[:emoji_id]
-          word_emoji = Emoji.find(emoji_id).image
-          @return.push(word_emoji)
+          @return.push(Emoji.find(keyword_hash[:emoji_id]).image)
         end
       end
     end
