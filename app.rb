cee954c652349805fa401c2e8b4067ca45067f71
Chris Clifford <cclifford82@gmail.com>
2016-12-21 10:33:06 -0800

    add created at to tweet display

--- app.rb
+++ app.rb
@@ -40,30 +40,6 @@ post('/tweet') do
   redirect('/user_search')
 end
 
-get('/user_search') do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
-  @tweets = []
-  @translated = []
-  erb(:user_search)
-end
-
-post('/user_search') do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
-  user_name = params['user_name']
-  @translated = []
-  language = params['language'].to_sym
-  @tweets = $twitter_client.user_timeline(user_name)
-  @tweets.each() do |tweet|
-    @translated.push(EasyTranslate.translate(tweet.text, :to => language))
-  end
-  if params['switch']
-    @target_user = params['target_user']
-  end
-  # $twitter_client.update(user_name)
-  # @tweets = $twitter_client.search(user_name, result_type: "recent").take(3).collect
-  erb(:user_search)
-end
-
 get('/keyword_search') do
   @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
   @tweets = []
@@ -73,15 +49,16 @@ end
 
 post('/keyword_search') do
   @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
-  search_term = params['search_term']
+  search_term = params['search-term']
   @translated = []
-  language = params['language'].to_sym
+  # language = params['language'].to_sym
   @tweets = $twitter_client.search(search_term, result_type: "recent").take(20).collect
   @tweets.each() do |tweet|
     tweet_text_with_info = []
     tweet_text_with_info.push(tweet.user.name)
     tweet_text_with_info.push(tweet.user.screen_name)
-    tweet_text_with_info.push(EasyTranslate.translate(tweet.text, :to => language))
+    tweet_text_with_info.push(tweet.created_at)
+    tweet_text_with_info.push(EasyTranslate.translate(tweet.text, :to => :spanish))
     @translated.push(tweet_text_with_info)
   end
   if params['switch']
