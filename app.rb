bb5b21d551112e8fa23bcafa958f23e52a1c59ad
Adam Zinder and Luke Philips <luke@>
2016-12-22 13:08:43 -0800

    easter egg added

--- app.rb
+++ app.rb
@@ -88,8 +88,37 @@ get '/emoji_tweet' do
   erb(:test)
 end
 get '/tweet_back' do
-
   @tweet_back = $twitter_client.update("@#{$twitter_client.mentions.first.user.user_name} #{$twitter_client.mentions.first.text.to_array}").text
   @you_tweeted = true
   erb(:test)
+end
+
+get '/keyword_search/soviet' do
+  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
+  @tweets = []
+  @translated = []
+  erb :keyword_search_soviet,:layout => :layout_soviet
+end
+
+post'/keyword_search/soviet' do
+  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
+  search_term = params['search-term']
+  @translated = []
+  # language = params['language'].to_sym
+  @tweets = $twitter_client.search(search_term, result_type: "recent").take(5).collect
+  @tweets.each() do |tweet|
+
+    tweet_text_with_info = {:user_name => tweet.user.name, :screen_name => tweet.user.screen_name,
+    :created_at => tweet.created_at, :text => tweet.text, :emoji => tweet.text.to_array,
+    :russian => (EasyTranslate.translate(tweet.text, :to => :russian).to_s),
+    :spanish => (EasyTranslate.translate(tweet.text, :to => :spanish)),
+    :japanese => (EasyTranslate.translate(tweet.text, :to => :japanese))}
+    @translated.push(tweet_text_with_info)
+  end
+  if params['switch']
+    @target_user = params['target_user']
+  end
+  # $twitter_client.update(user_name)
+  # @tweets = $twitter_client.search(user_name, result_type: "recent").take(3).collect
+  erb :keyword_search_soviet,:layout => :layout_soviet
 end
