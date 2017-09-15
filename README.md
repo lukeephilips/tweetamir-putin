aa072851b466bdb3b6fbf7a2cfcead98f04978e3
Luke <luke.e.philips@gmail.com>
2017-09-14 18:45:05 -0700

    added new twitter handle, wired off EasyTranslate

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# _@Twittamir_Putin_
+# _@TwittamirPutin_
 
 #### By _*Ezra Sandzer-Bell, Chris Clifford, Luke Philips, and Adam Zinder*_
 
 ## Description
 Twittamir Putin is a web app for searching and translating twitter, as well as translating and tweeting user created content. It covers English, Spanish, Russian, Japanese and emojis. It uses the Twitter API, Google API, and our own custom emoji translator. Deployed live at http://twittamir-putin.herokuapp.com.
 
-The user can search Twitter for tweets by keyword or user, then display all translations. The user can then retweet any translation from the @Twittamir_Putin handle. The user can also write their own content to translate, with the option to tweet it from the @twittamir-putin handle.
+The user can search Twitter for tweets by keyword or user, then display all translations. The user can then retweet any translation from the @TwittamirPutin handle. The user can also write their own content to translate, with the option to tweet it from the @TwittamirPutin handle.
 
 The emoji translator uses a db of all emojis with corresponding tags. Sentences are parsed into words and then each word is run against the db for a case insensitive match. If no match is found, all substrings of the word are run against the db, and any substrings matches are replaced by their emoji in order of substring length (i.e.: Chickenhouse => ["chicken"+"house"] => 🐔
 🏘, and the substring match of "chick" is superceded by "chicken due to substring length").
--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ require("pg")
 require('easy_translate')
 require('dotenv')
 require('pry')
+require('byebug')
 require('./lib/emoji')
 require('./lib/keyword')
 require('./lib/sentence')
@@ -18,15 +19,8 @@ require('./lib/add_tags')
 Dotenv.load
 
 get('/') do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
-  @tweets = []
-  @translated = []
+  user_tweets
   erb(:index)
-end
-
-
-post('/') do
-
 end
 
 post('/tweet') do
@@ -57,27 +51,26 @@ post('/tweet_search_soviet') do
 end
 
 get('/keyword_search') do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
-  @tweets = []
-  @translated = []
+  user_tweets
   erb(:keyword_search)
 end
 
 post('/keyword_search') do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
+  user_tweets
   search_term = params['search-term']
-  @translated = []
-  # language = params['language'].to_sym
-
   if !search_term.count("a-zA-Z0-9").zero?
     @tweets = $twitter_client.search(search_term, result_type: "recent").take(5).collect
     @tweets.each() do |tweet|
-
-      tweet_text_with_info = {:user_name => tweet.user.name, :screen_name => tweet.user.screen_name,
-      :created_at => tweet.created_at, :text => tweet.text, :emoji => tweet.text.to_array,
-      :russian => (EasyTranslate.translate(tweet.text, :to => :russian).to_s),
-      :spanish => (EasyTranslate.translate(tweet.text, :to => :spanish)),
-      :japanese => (EasyTranslate.translate(tweet.text, :to => :japanese))}
+      tweet_text_with_info = {
+        :user_name => tweet.user.name,
+        :screen_name => tweet.user.screen_name,
+        :created_at => tweet.created_at,
+        :text => tweet.text,
+        :emoji => tweet.text.to_array
+        # :russian => EasyTranslate.translate(tweet.text, :to => :russian).to_s,
+        # :spanish => EasyTranslate.translate(tweet.text, :to => :spanish),
+        # :japanese => EasyTranslate.translate(tweet.text, :to => :japanese)
+      }
       @translated.push(tweet_text_with_info)
     end
   end
@@ -86,23 +79,27 @@ post('/keyword_search') do
   end
   # $twitter_client.update(user_name)
   # @tweets = $twitter_client.search(user_name, result_type: "recent").take(3).collect
+  byebug
   erb(:keyword_search)
 end
 
 get '/emoji' do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
+  user_tweets
   erb(:emoji)
 end
 
 post '/emoji' do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
+  user_tweets
   tweet = params['sentence']
 
-  @return = {:user_name => $twitter_client.user.name, :screen_name => $twitter_client.user.screen_name,
-  :created_at => Time.now, :text => tweet, :emoji => tweet.to_array,
-  :russian => (EasyTranslate.translate(tweet, :to => :russian)),
-  :spanish => (EasyTranslate.translate(tweet, :to => :spanish)),
-  :japanese => (EasyTranslate.translate(tweet, :to => :japanese))}
+  @return = {
+    :user_name => $twitter_client.user.name,
+    :screen_name => $twitter_client.user.screen_name,
+    :created_at => Time.now, :text => tweet, :emoji => tweet.to_array,
+    # :russian => EasyTranslate.translate(tweet, :to => :russian),
+    # :spanish => EasyTranslate.translate(tweet, :to => :spanish),
+    # :japanese => EasyTranslate.translate(tweet, :to => :japanese)
+  }
 
   erb(:emoji)
 end
@@ -118,16 +115,14 @@ get '/tweet_back' do
 end
 
 get '/keyword_search/soviet' do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
-  @tweets = []
-  @translated = []
-  erb :keyword_search_soviet,:layout => :layout_soviet
+  user_tweets
+  erb :keyword_search_soviet, :layout => :layout_soviet
 end
 
 post'/keyword_search/soviet' do
-  @user_tweets = $twitter_client.user_timeline('Twittamir_Putin')
+  user_tweets
+
   search_term = params['search-term']
-  @translated = []
   # language = params['language'].to_sym
   @tweets = $twitter_client.search(search_term, result_type: "recent").take(5).collect
   @tweets.each() do |tweet|
@@ -139,10 +134,15 @@ post'/keyword_search/soviet' do
     :japanese => (EasyTranslate.translate(tweet.text, :to => :japanese))}
     @translated.push(tweet_text_with_info)
   end
+
   if params['switch']
     @target_user = params['target_user']
   end
-  # $twitter_client.update(user_name)
-  # @tweets = $twitter_client.search(user_name, result_type: "recent").take(3).collect
   erb :keyword_search_soviet,:layout => :layout_soviet
+end
+
+def user_tweets
+  @user_tweets = $twitter_client.user_timeline('TwittamirPutin')
+  @tweets = []
+  @translated = []
 end
